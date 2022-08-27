c4c7ac9b996affde6842f2ef9817441045b5f73e
khanhha <khanhhadsdl@gmail.com>
2022-08-27 10:33:07 +0700

    BT1: Cài đặt môi trường

---
+++ test.rb
@@ -0,0 +1,11 @@
+puts "Xin chào Nguyễn Thị Khánh Hạ - 1911505310117"
+
+puts "Nhập 2 số: "
+num1 = gets
+num2 = gets
+
+if num1 > num2
+    puts "Số lớn hơn là " + num1
+else
+    puts "Số lớn hơn là " + num2
+end
